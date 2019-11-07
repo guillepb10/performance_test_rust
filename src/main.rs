aba87030f6b74a94df0b65feb2c87a7469f60448
Guillermo Priego Blanco <guillermo.priego.next@bbva.com>
2019-11-07 19:27:21 +0100

    service logic into future

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use hyper::{Body, Request, Response, Server};
 use hyper::rt::Future;
 use hyper::service::service_fn;
+use hyper::{Body, Request, Response, Server};
 use hyper::{Method, StatusCode};
 
 use rand;
-use rand::{RngCore};
+use rand::RngCore;
 
-use futures::future;
+use futures::{future, lazy};
 
 use regex::Regex;
 
 use serde::Serialize;
 
-type BoxFut = Box<dyn Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
 #[derive(Serialize)]
 struct ValueList {
-    list: Vec<u32>
+    list: Vec<u32>,
 }
 
 impl ValueList {
@@ -28,29 +28,34 @@ impl ValueList {
     }
 }
 
-
 fn main() {
     let addr = ([127, 0, 0, 1], 8080).into();
 
     let server = Server::bind(&addr)
-                    .serve(|| {
-                        service_fn(route_rand_number)
-                    })
-                    .map_err(|e| eprintln!("server error: {}", e));
+        .serve(|| service_fn(route_rand_number))
+        .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
-
 }
 
 fn route_rand_number(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
+    let mut response = Response::builder();
     let path = Regex::new("^/random/([0-9]*)$").unwrap();
     if req.method() == Method::GET && path.is_match(req.uri().path()) {
-        response = rand_numbers(path.captures(req.uri().path()).unwrap().get(1).map(|n| n.as_str().parse::<u16>().unwrap()).unwrap())
+        let size = path
+            .captures(req.uri().path())
+            .unwrap()
+            .get(1)
+            .map(|v| v.as_str().parse::<u16>().unwrap())
+            .unwrap();
+        return Box::new(lazy(move || Ok(rand_numbers(size))));
     } else {
-        *response.status_mut() = StatusCode::NOT_FOUND;
+        let response = response
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap();
+        return Box::new(future::ok(response));
     }
-    Box::new(future::ok(response))
 }
 
 fn rand_numbers(n: u16) -> Response<Body> {
